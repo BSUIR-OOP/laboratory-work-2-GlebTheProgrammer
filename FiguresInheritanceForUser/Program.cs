64753e35858a9a6697264f829f9f6eca1fafabcc
Gleb Sukristik <gleb15a@gmail.com>
2022-03-23 21:21:52 +0300

    Lab2 implementation. Added oportunities for user to add, delete, print figures

--- FiguresInheritanceForUser/Program.cs
+++ FiguresInheritanceForUser/Program.cs
@@ -47,6 +47,185 @@ namespace FiguresInheritanceForUser
                     Console.ReadLine();
                     break;
 
+
+                case 2:
+                    while (true)
+                    {
+                        ShowExistingMethods();
+                        int choice = int.Parse(Console.ReadLine());
+
+                        if (choice == 0)  // Exit
+                            break;
+
+                        if (choice == 1)  // Add figure
+                        {
+                            ShowPossibleFigures();
+                            int action = int.Parse(Console.ReadLine());
+
+                            switch (action)
+                            {
+                                // Back
+                                case 0:
+                                    break;
+
+                                // Figure
+                                case 1:
+                                    figuresContainer.Add(new Figure());
+                                    Console.WriteLine("Done");
+                                    Console.ReadLine();
+                                    break;
+
+                                // Line segment
+                                case 2:
+                                    Console.Write("Length = ");
+                                    float length = float.Parse(Console.ReadLine());
+                                    figuresContainer.Add(new LineSegment(length));
+                                    Console.WriteLine("Done");
+                                    Console.ReadLine();
+                                    break;
+
+                                // Square
+                                case 3:
+                                    Console.Write("Side = ");
+                                    float side = float.Parse(Console.ReadLine());
+                                    figuresContainer.Add(new Square(side));
+                                    Console.WriteLine("Done");
+                                    Console.ReadLine();
+                                    break;
+
+                                // Cube
+                                case 4:
+                                    Console.Write("Side = ");
+                                    side = float.Parse(Console.ReadLine());
+                                    figuresContainer.Add(new Cube(side));
+                                    Console.WriteLine("Done");
+                                    Console.ReadLine();
+                                    break;
+
+                                // Rectangle
+                                case 5:
+                                    Console.Write("Length = ");
+                                    length = float.Parse(Console.ReadLine());
+                                    Console.Write("Width = ");
+                                    float width = float.Parse(Console.ReadLine());
+                                    figuresContainer.Add(new Rectangle(length, width));
+                                    Console.WriteLine("Done");
+                                    Console.ReadLine();
+                                    break;
+
+                                // Rhombus
+                                case 6:
+                                    Console.Write("Length = ");
+                                    length = float.Parse(Console.ReadLine());
+                                    Console.Write("Heigh = ");
+                                    float heigh = float.Parse(Console.ReadLine());
+                                    figuresContainer.Add(new Rhombus(length, heigh));
+                                    Console.WriteLine("Done");
+                                    Console.ReadLine();
+                                    break;
+
+                                // Triangle
+                                case 7:
+                                    Console.Write("Side A = ");
+                                    float sideA = float.Parse(Console.ReadLine());
+                                    Console.Write("Side B = ");
+                                    float sideB = float.Parse(Console.ReadLine());
+                                    Console.Write("Side C = ");
+                                    float sideC = float.Parse(Console.ReadLine());
+                                    figuresContainer.Add(new Triangle(sideA, sideB, sideC));
+                                    Console.WriteLine("Done");
+                                    Console.ReadLine();
+                                    break;
+
+                                // Triangle prism
+                                case 8:
+                                    Console.Write("Side A = ");
+                                    sideA = float.Parse(Console.ReadLine());
+                                    Console.Write("Side B = ");
+                                    sideB = float.Parse(Console.ReadLine());
+                                    Console.Write("Side C = ");
+                                    sideC = float.Parse(Console.ReadLine());
+                                    Console.Write("Heigh = ");
+                                    heigh = float.Parse(Console.ReadLine());
+                                    figuresContainer.Add(new TrianglePrism(sideA, sideB, sideC, heigh));
+                                    Console.WriteLine("Done");
+                                    Console.ReadLine();
+                                    break;
+
+                                // Circle
+                                case 9:
+                                    Console.Write("Radius = ");
+                                    float radius = float.Parse(Console.ReadLine());
+                                    figuresContainer.Add(new Circle(radius));
+                                    Console.WriteLine("Done");
+                                    Console.ReadLine();
+                                    break;
+
+                                // Cylinder
+                                case 10:
+                                    Console.Write("Radius = ");
+                                    radius = float.Parse(Console.ReadLine());
+                                    Console.Write("Heigh = ");
+                                    heigh = float.Parse(Console.ReadLine());
+                                    figuresContainer.Add(new Cylinder(radius, heigh));
+                                    Console.WriteLine("Done");
+                                    Console.ReadLine();
+                                    break;
+
+                                // Cone
+                                case 11:
+                                    Console.Write("Radius = ");
+                                    radius = float.Parse(Console.ReadLine());
+                                    Console.Write("Heigh = ");
+                                    heigh = float.Parse(Console.ReadLine());
+                                    figuresContainer.Add(new Cone(radius, heigh));
+                                    Console.WriteLine("Done");
+                                    Console.ReadLine();
+                                    break;
+
+                                // Not found exception
+                                default:
+                                    Console.WriteLine("Action does not exist. Please, type an existing one.");
+                                    Console.ReadLine();
+                                    break;
+                            }
+                        }
+
+                        if (choice == 2)  // Delete figure by ID
+                        {
+                            Console.Clear();
+                            Console.Write("Figure ID: ");
+                            uint Id = uint.Parse(Console.ReadLine());
+                            DeleteFigureByID(figuresContainer, Id);
+                            Console.ReadLine();
+                        }
+
+                        if (choice == 3)  // Print figure by ID
+                        {
+                            Console.Clear();
+                            Console.Write("Figure index: ");
+                            uint Id = uint.Parse(Console.ReadLine());
+                            Console.WriteLine();
+
+                            PrintFigureByID(figuresContainer, Id);
+                            Console.WriteLine("Done");
+                            Console.ReadLine();
+                        }
+
+                        if (choice == 4)  // Print all figures
+                        {
+                            Console.Clear();
+                            PrintAllFigures(figuresContainer);
+                            Console.WriteLine("Done");
+                            Console.ReadLine();
+                        }
+                    }
+                    Console.Clear();
+                    Console.WriteLine("Progarm was executed. Press ENTER button.");
+                    Console.ReadLine();
+                    break;
+
+
                 default:
                     Console.WriteLine("You've typed wrong number. Program was executed. Press ENTER button.");
                     Console.ReadLine();
